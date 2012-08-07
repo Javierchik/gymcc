2b97a29f2f59dd8875b497e55bb61d7e171bda60
Javierchik <javier.siado@koombea.com>
2012-08-07 13:24:36 -0500

    initial commit

---
+++ Gemfile
@@ -0,0 +1,19 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.7'
+gem "twitter-bootstrap-rails", "~> 2.1.1"
+
+group :development do
+  gem 'mysql2'
+end
+
+group :production do
+  gem 'ruby-odbc', "~> 0.99994"
+  gem 'activerecord-sqlserver-adapter','~> 3.2.8'
+end
+
+group :assets do
+  gem 'uglifier', '>= 1.0.3'
+end
+
+gem 'jquery-rails'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gymcc::Application.config.secret_token = 'de60405cc4b498898b00974c783bd57c8dca91c407aba47a490df0e0128f66dd528d5e5a762b950e4575d8cebc17023728b9d002aef595d916ccc3f674673ac3'
