f3eccc31d225c992cff12e0823d002f890c7d79a
JAvierchik <javier.siadokkoombea.com>
2012-09-05 14:11:38 -0500

    encoding charachters

--- config/application.rb
+++ config/application.rb
@@ -35,13 +35,13 @@ module Gymcc
     config.i18n.default_locale = :es
 
     # Configure the default encoding used in templates for Ruby 1.9.
-    config.encoding = "utf-8"
+    config.encoding = "UTF-8"
 
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
 
-    # Enable escaping HTML in JSON.
+    # Enable escaping HTML in JSON. 
     config.active_support.escape_html_entities_in_json = true
 
     # Use SQL instead of Active Record's schema dumper when creating the database.
