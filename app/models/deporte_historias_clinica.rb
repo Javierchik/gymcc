8671939d2e5488db7cc729832d742210c6f766a0
Javierchik <javiersiado@gmail.com>
2012-09-05 20:00:37 -0500

    adding missing fields for historia clinica and groupo pacientes _id

--- app/models/deporte_historias_clinica.rb
+++ app/models/deporte_historias_clinica.rb
@@ -1,7 +1,9 @@
 class DeporteHistoriasClinica < ActiveRecord::Base
   belongs_to :socio
 
-  attr_accessible :nombre_acompanante, :telefono_acompanante, :nombre_responsable, :socio_id
+  GRUPO_PACIENTE = [["Diabetico e Hipertenso", 0], ["Poblacin especial", 1], ["Persona aparentemente sana", 2], ["Otras patologias", 3]]
+
+  attr_accessible :nombre_acompanante, :telefono_acompanante, :nombre_responsable, :socio_id, :grupo_paciente_id
   attr_accessible :telefono_responsable, :parentesco_responsable, :nombre_aseguradora, :tipo_aseguradora, :tipo_sangre
   attr_accessible :motivo_consulta, :enfermedad_actual, :ante_personales_patologicos, :ante_gino_obstetricos, :ante_quirurgicos, :ante_alergicos, :ante_familiares, :frecuencia_cardiaca, :frecuencia_respiratoria, :tension_arterial, :temperatura, :peso_actual, :talla, :imc, :porcentage_graso, :ccc, :cardiopulmonar, :abdomen, :extremidades, :snc, :diagnostico, :tratamiento, :recomendaciones
 
